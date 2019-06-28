4ba6be90111c3e28652f526f7885d8d76d4531ce
kierepkae <kierepkaedyta@gmail.com>
2019-06-28 12:35:54 +0100

    added public methid withdraw and added comments

--- banking.rb
+++ banking.rb
@@ -6,6 +6,7 @@ class Account
       @balance = balance
     end
 
+    # private methods
 
     private
     def pin
@@ -17,10 +18,23 @@ class Account
       return "Access denied: incorrect PIN."
     end
 
+# public methods
+
     public
     def display_balance(pin_number)
       if (pin_number==pin)
+        balance -= amount
         puts "Balance: $#{@balance}."
+      else
+        puts pin_error
+      end
+    end
+
+
+    public
+    def withdraw(pin_number,amount)
+      if(pin_number == pin)
+        puts "Withdrew #{amount}. New balance: $#{@balance}."
       else
         puts pin_error
       end
